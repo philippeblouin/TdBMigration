60e8cd955d29087cdff8e220c12a1bcd67bf3b08
Philippe Blouin <philippe.blouin@ibm.com>
2025-01-14 15:36:24 -0500

    Premier

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+
+def loadJsonPerformance(chemin):
+    pass
+
+def loadJsonAvancement(chemin):
+    pass
+
+def loadJsonApplication(chemin):
+    pass
+
+
+def main():
+    pass
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
